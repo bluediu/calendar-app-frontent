973988e8d6f79e2fc1dbd94c25d7f90bdb2f53ec
jrivas <josuerivas2311@gmail.com>
2024-08-10 12:01:32 -0600

    Add new property

--- src/apps/Auth/context/slices/auth.slice.ts
+++ src/apps/Auth/context/slices/auth.slice.ts
@@ -5,6 +5,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IAuthState, IAuthUser } from '../../types';
 
 const initialState: IAuthState = {
+  isPending: false,
   status: 'checking',
   user: {},
   errorMessage: undefined,
@@ -15,12 +16,14 @@ export const authSlice = createSlice({
   initialState,
   reducers: {
     onChecking: (state: IAuthState) => {
+      state.isPending = true;
       state.status = 'checking';
       state.user = {};
     },
     onLogin: (state: IAuthState, action: PayloadAction<IAuthUser>) => {
       state.status = 'authenticated';
       state.user = action.payload;
+      state.isPending = false;
     },
     onLogout: (
       state: IAuthState,
@@ -33,8 +36,16 @@ export const authSlice = createSlice({
     clearErrorMessage: (state) => {
       state.errorMessage = undefined;
     },
+    setPendingStatus: (state, action: PayloadAction<boolean>) => {
+      state.isPending = action.payload;
+    },
   },
 });
 
-export const { onChecking, onLogin, onLogout, clearErrorMessage } =
-  authSlice.actions;
+export const {
+  onChecking,
+  onLogin,
+  onLogout,
+  clearErrorMessage,
+  setPendingStatus,
+} = authSlice.actions;
