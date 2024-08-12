c3e6ff3b87899640ba8f2606ed402891a6f7a206
jrivas <josuerivas2311@gmail.com>
2024-08-12 11:29:54 -0600

    Create event localizer

---
+++ src/apps/Calendar/helpers/localizer.ts
@@ -0,0 +1,17 @@
+import { dateFnsLocalizer, DateLocalizer } from 'react-big-calendar';
+
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+
+import esES from 'date-fns/locale/es';
+
+const locales = {
+  es: esES,
+};
+
+export const localizer: DateLocalizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
